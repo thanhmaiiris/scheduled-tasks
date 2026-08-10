cf321aafb4050d3bf46e5d8b5719a349394d5471
thanhmaiiris <thanhmaiiris2407@gmail.com>
2026-08-11 02:09:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import datetime as dt
 import random
 import smtplib
 import os
-WEEKLY_SENDING_DAY = 0
+WEEKLY_SENDING_DAY = [0,1,2,3,4,5,6]
 to_mail = "student232616@ptnk.edu.vn"
 my_mail = os.environ.get("MY_EMAIL")
 password = os.environ.get("MY_PASSWORD")
@@ -12,12 +12,12 @@ def check_weekday():
     now = dt.datetime.now()
     weekday = now.weekday()
     print(weekday)
-    if weekday == WEEKLY_SENDING_DAY:
+    if weekday in WEEKLY_SENDING_DAY:
         return True
     else:
         return False
 
-with open("quotes.txt") as file:
+with open("quotes.txt", encoding = "utf_8") as file:
     quotes = file.readlines()
     print(quotes)
 
@@ -27,7 +27,7 @@ def get_random_quote():
     return random_quote
 
 if check_weekday() == True:
-    with smtplib.SMTP("smtp.gmail.com") as connection:
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
         connection.starttls()
         connection.login(my_mail, password=password)
         connection.sendmail(
